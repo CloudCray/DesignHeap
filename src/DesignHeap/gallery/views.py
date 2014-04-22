d49ba4ec4639f2c5fe0643b76c8508198d309cba
Cloud Cray <cloudcray@gmail.com>
2014-04-22 13:12:46 -0400

    Restructured to make Heroku friendly

--- src/DesignHeap/gallery/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-
-from django.shortcuts import render_to_response
-from DesignHeap.settings import MEDIA_ROOT
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
-
-import re
-import base64
-dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
