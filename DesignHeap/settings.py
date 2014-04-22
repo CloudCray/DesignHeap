a8694a28071de40c237e9d5d82fd159294990fdd
Cloud Cray <cloudcray@gmail.com>
2014-04-22 13:20:25 -0400

    Restructured to make Heroku friendly, but for real this time

--- DesignHeap/settings.py
+++ DesignHeap/settings.py
@@ -118,7 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'C:/Users/Cloud/Dropbox/Projects/DesignHeap/src/DesignHeap/base/templates/',
+    os.path.join(SETTINGS_DIR, "base", "templates"),
 )
 
 INSTALLED_APPS = (
