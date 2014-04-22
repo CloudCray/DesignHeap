ba4942df4b96e97f7fd5b93d496eca1c133cce02
Cloud Cray <cloudcray@gmail.com>
2014-04-22 16:19:33 -0400

    fixing static files

--- DesignHeap/settings.py
+++ DesignHeap/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SETTINGS_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -87,9 +87,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SETTINGS_DIR, "static"),
-    os.path.join(SETTINGS_DIR, "static", "styles"),
-)
+    )
 
 # List of finder classes that know how to find static files in
 # various locations.
